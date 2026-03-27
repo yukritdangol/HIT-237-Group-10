2cbf805cfa45db6bbda6fd7c366ab9a3924c013d
Aarjit Acharya <aarjitacharya@gmail.com>
2026-03-27 20:37:43 +0930

    Added Location model and foreign key relationship with housing

--- housing_project/housing/admin.py
+++ housing_project/housing/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Housing
+from .models import Housing, Location
 
 admin.site.register(Housing)
+admin.site.register(Location)
--- housing_project/housing/models.py
+++ housing_project/housing/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
-class Housing(models.Model):
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    regiom = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Housing (models.Model):
     title = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     price = models.IntegerField()
     description = models.TextField()
     is_available = models.BooleanField(default=True)
