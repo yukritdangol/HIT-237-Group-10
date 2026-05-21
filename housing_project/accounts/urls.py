68cfb4f920a55c50cccba1feb39aecf4f1a26784
yukritdangol <yukritdangol@gmail.com>
2026-05-21 11:56:25 +0930

    Yukrit Commit

--- housing_project/accounts/urls.py
+++ housing_project/accounts/urls.py
@@ -1,5 +1,35 @@
 from django.urls import path
-from . import views
+
+from .views import (
+    signup_view,
+    login_view,
+    logout_view,
+    dashboard_view,
+)
 
 urlpatterns = [
+
+    path(
+        'signup/',
+        signup_view,
+        name='signup'
+    ),
+
+    path(
+        'login/',
+        login_view,
+        name='login'
+    ),
+
+    path(
+        'logout/',
+        logout_view,
+        name='logout'
+    ),
+
+    path(
+        'dashboard/',
+        dashboard_view,
+        name='dashboard'
+    ),
 ]
--- housing_project/housing_project/settings.py
+++ housing_project/housing_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'housing',
-    'accounts',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'housing_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
